a206c5e2a55353ec79479e576449930d37a5f9b7
Harsha <khurdhulaharshavardhan@gmail.com>
2023-12-09 09:33:24 -0500

    creates a library directory for custom modules of this task.

---
+++ library/pipeline.py
@@ -0,0 +1,12 @@
+
+
+class Pipeline():
+    """
+    This Custom module aims to preprocess user input text from end to end.
+    - It normalizes the input text:
+        - Lowers entire text into lowercase for case consistency.
+        - Extracts only textual information discarding the rest.
+        - Removes stop words.
+    - And then generates word embeddings.
+    """
+    pass
