9db5514a5ec2ba410d3473b841d14d40ed00bd07
Harsha <khurdhulaharshavardhan@gmail.com>
2023-12-09 11:41:50 -0500

    Pipeline can now normalize text. And provide the target string.

--- library/pipeline.py
+++ library/pipeline.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
+import re
 
 class Pipeline():
     """
@@ -16,12 +17,25 @@ class Pipeline():
 
     def __init__(self) -> None:
         try:
-            nltk.download('stopwords')
-            nltk.download('punkt')
-
-            self.stop_words = set(stopwords.words("english"))
+            self.stop_words = set(['i', 'feel', 'and', 'to', 'the', 'a', 'feeling', 'that', 'of', 'my', 'in', 'it', 'like', 'so', 'for', 'im', 'me', 'but', 'was', 'have'])
         except Exception as e:
             print("[ERR] The following error occured while trying to Pre-Process the text: "+str(e))
+
+    def normalize_text(self, text:str) -> str:
+        """
+        This method normalizes the given text:
+        - Lowers entire text into lowercase for case consistency.
+        - Replaces words like didn't with didnt.
+        - Extracts only textual information discarding the rest.
+        """
+        try:
+            text = text.lower()
+            text = text.replace("'","")
+            text = re.findall("[a-z]+", text)
+            text = " ".join(text)
+            return text
+        except Exception as e:
+            print("[ERR] The following error occured while trying to normalize the text: "+str(e))
 
     def remove_most_frequent_stopword(self, text:str) -> str:
         """
@@ -37,3 +51,11 @@ class Pipeline():
         except Exception as e:
             print("[ERR] The following error occured while trying to removing the most frequent stop words from the string: "+str(e))
 
+    def transform_text(self, text:str) -> str:
+        """
+        Implements both normalize and stopwords method to get the target string.
+        """
+        text = self.normalize_text(text= text)
+        text = self.remove_most_frequent_stopword(text= text)
+        return text
+    
